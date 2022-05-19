8b0caa0ab068a2e22a023b425f2d87ac65f99fb6
Aviciena Santoso <avi.santoso@gmail.com>
2022-05-19 10:15:35 +0800

    rename files according to go conventions

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -16,7 +16,7 @@ echo "$cov_res"
 
 cat $cov_out | grep -i -v $ignored_files > $cov_out
 
-gcov2lcov -infile=coverage.out -outfile=lcov.info
+gcov2lcov -infile=$cov_out -outfile=lcov.info
 
 if echo $cov_res | grep -i -q 'no test files';
     then
